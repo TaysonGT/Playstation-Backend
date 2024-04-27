836e4aefef1859f51a819c4c3ad7de6e69f92b06
Tayson <mohamedmanno2003@gmail.com>
2024-04-27 16:49:52 +0300

    plaese

--- dist/src/main.js
+++ dist/src/main.js
@@ -33,7 +33,7 @@ app.use(express_1.default.urlencoded({
     extended: true
 }));
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://playstation-frontend.vercel.app"); // "*"
+    res.header("Access-Control-Allow-Origin", "https://playstation-frontend.vercel.app"); // "*"
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
